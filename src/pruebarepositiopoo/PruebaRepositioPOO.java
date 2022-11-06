68fcf0312de8ea4939dc1e5ed410346d1c5d3a11
marco50406 <matnudi504@gmail.com>
2022-11-06 12:43:14 -0600

    Primer commit para repositorio remoto -

--- src/pruebarepositiopoo/PruebaRepositioPOO.java
+++ src/pruebarepositiopoo/PruebaRepositioPOO.java
@@ -15,6 +15,8 @@ public class PruebaRepositioPOO {
      */
     public static void main(String[] args) {
         System.out.println("Prueba de Repositorio");
+        
+        System.out.println("Haciendo cambios");
     }
     
 }
